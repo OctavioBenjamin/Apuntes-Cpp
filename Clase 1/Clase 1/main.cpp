4c7498ae54ca2f80d91a0a6473f7b5cf78efda47
OctavioBenjamin <benjaminmen55@gmail.com>
2021-05-15 12:07:36 -0300

    Se agrego la segunda clase

---
+++ Clase 1/Clase 1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    //Variables
+    int numero = 1; //Numeros enteros
+
+    float pi = 3.14; //Numeros con coma
+
+    bool verdadero = true; // Valores Verdaderos (True) y Falsos (False)
+
+    string miNombre = "Octavio"; //Variables con cadena de texto
+
+    cout << miNombre << endl; // Cout para imprimir en consola
+    cout << "Hola mundo" << endl;
+    return 0;
+}
